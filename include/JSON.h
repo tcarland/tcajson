753b06066c2687a5749ba8b2af776c9e5e540d3a
Timothy C. Arland <tcarland@gmail.com>
2025-01-28 16:27:54 -0700

    update to stdc++23 vers/lic bump container updates

--- include/JSON.h
+++ include/JSON.h
@@ -1,7 +1,7 @@
 /**
   * @file JSON.h
   *
-  * Copyright (c) 2008-2024 Timothy Charlton Arland <tcarland@gmail.com>
+  * Copyright (c) 2008-2025 Timothy Charlton Arland <tcarland@gmail.com>
   *
   * @section LICENSE
   *
@@ -37,7 +37,7 @@
 namespace tcajson {
 
 
-#define TCAJSON_VERSION    "v2.4.3"
+#define TCAJSON_VERSION    "v2.5.0"
 #define TCAJSON_ERRSTRLEN   48
 
 
